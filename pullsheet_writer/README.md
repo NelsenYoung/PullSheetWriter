5fb7247c70613888c46350b75fd902f9d07ae0c4
NelsenYoung <127802446+NelsenYoung@users.noreply.github.com>
2023-09-17 14:54:29 -0700

    Add files via upload

---
+++ pullsheet_writer/README.md
@@ -0,0 +1,14 @@
+Author: Nelsen Young
+Date: 9/17/2023
+Purpose: Write pullsheets for Vitis Vending
+
+Setup:
+1. Setup a virtual environment
+2. install google chrome web driver
+3. import selenium using pip install selenium
+4. import chrome webdriver manager using pip install webdriver-manager
+
+Running the Program:
+Enter the machine id, which is the first column on the greenlite machine sales table, to select the machine you would like a pull sheet written for. 
+
+You must click the sign in button as the program will not sign in for you. However, the login information will automatically be inputted.
---
+++ pullsheet_writer/greenlite_scraper.py
@@ -0,0 +1,70 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service as ChromeService 
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import csv
+from bs4 import BeautifulSoup as bs 
+import requests 
+
+desired_macchine_id = input("Please enter the machine you are servicing: ")
+
+# Greenlite Credentials
+username = "fredcougar17@gmail.com"
+password = "Tipper05"
+
+#install the Chrome driver
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+# Go to Greenlite login page
+URL = "https://greenlite.mycantaloupe.com/login/" 
+driver.get(URL)
+
+# login to Greenlite
+driver.find_element("id", "UserName").send_keys(username)
+driver.find_element("id", "Password").send_keys(password)
+button = driver.find_element("id", "signInButton")
+button.click()
+
+time.sleep(10)
+
+# Find the desired machine
+view_container = driver.find_element(By.ID, "viewContainer")
+machine_id = view_container.find_element(By.LINK_TEXT, desired_macchine_id)
+machine_id.click()
+
+
+time.sleep(5)
+
+
+machine_container = driver.find_element(By.ID, "machineDetailTabControl")
+
+print("coil - item count")
+# Parse the levels of the current machine
+level_string = "ctl00_mainContentPlaceHolder_machineDetailInventory_currentPlanogramViewer_dgGeneralInventoryView_ctl02_LblLevel"
+sales_string = "ctl00_mainContentPlaceHolder_machineDetailInventory_currentPlanogramViewer_dgGeneralInventoryView_ctl02_sinceLastRestockQuantityLabel"
+coil_string = "ctl00_mainContentPlaceHolder_machineDetailInventory_currentPlanogramViewer_dgGeneralInventoryView_ctl02_LblCoil"
+items = machine_container.find_elements(By.CLASS_NAME, "itemDetailLink")
+
+i = 2
+for item in items:
+
+    if(i >= 10):
+        count = machine_container.find_element(By.ID, level_string.replace("02", str(i)))
+        sales = machine_container.find_element(By.ID, sales_string.replace("02", str(i)))
+        coil = machine_container.find_element(By.ID, coil_string.replace("02", str(i)))
+    else:
+        count = machine_container.find_element(By.ID, level_string.replace("2", str(i)))
+        sales = machine_container.find_element(By.ID, sales_string.replace("2", str(i)))
+        coil = machine_container.find_element(By.ID, coil_string.replace("2", str(i)))
+    
+
+    if(int(count.text) < 3 or int(sales.text) > 2):
+        if(int(sales.text) == 0):
+            print(coil.text, " - ", item.text, 2)
+        else:
+            print(coil.text, " - ", item.text, int(sales.text) + 1)
+    
+    i = i + 1
